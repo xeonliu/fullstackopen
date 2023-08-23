d3808282729f64eb797742202c51b4c5f6ee579d
Your Name <you@example.com>
2023-08-23 20:24:39 +0800

    Step3

---
+++ homework/courseinfo/src/App.js
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  console.log(props);
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+      />
+    </div>
+  );
+};
+
+export default App;
