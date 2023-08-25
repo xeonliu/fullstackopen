5cd6b017d687261c35caf8db9029c048d6a61abf
Your Name <you@example.com>
2023-08-25 09:49:28 +0800

    decreaseByOne

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,51 +1,25 @@
 import { useState } from "react";
 
-// Further deconstruct :
-// const Hello = ({name,age})=>{}
-const Hello = (props) => {
-  // deconstruct
-  const { name, age } = props;
-  const bornYear = () => {
-    // why new?
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
+const Display = (props) => {
+  return <div>{props.counter}</div>;
 };
 
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  const handleClick = () => {
-    console.log("clicked");
-    setCounter(counter + 1);
-  };
-
-  const now = new Date();
-  const a = 10;
-  const b = 20;
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={handleClick}>plus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
-      <p>Hello world, it is {now.toDateString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
-      <h1>Greetings</h1>
-      <Hello name="GG" age={a} />
-      <Hello name="SB" age={b} />
-      {/* <Hello /> */}
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={decreaseByOne} text="minus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
   );
 };
