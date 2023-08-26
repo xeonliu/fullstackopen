0c54d66eeb5e7e73df476e93a764e3f042d8ce47
Your Name <you@example.com>
2023-08-26 11:57:06 +0800

    part2-2

--- part2/src/App.js
+++ part2/src/App.js
@@ -9,11 +9,25 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
 
   const addNote = (event) => {
     event.preventDefault();
-    console.log("button clicked", event.target);
-    //即form
+    console.log("button clicked", event.target); //即form
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
   };
 
   //必须为Note组件定义key属性而不是li
@@ -26,7 +40,7 @@ const App = (props) => {
         ))}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
