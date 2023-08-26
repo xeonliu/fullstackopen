e286ade1771359a94c3d72e1251ec6d92a413d7f
Your Name <you@example.com>
2023-08-26 09:50:24 +0800

    key

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,13 +1,18 @@
+const Note = ({ note }) => {
+  return <li>{note.content}</li>;
+};
+
 const App = (props) => {
   const { notes } = props;
 
+  //必须为Note组件定义key属性而不是li
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => (
+          <Note key={note.id} note={note}></Note>
+        ))}
       </ul>
     </div>
   );
