c7a7febe225c378edc6cb9046f03a7f557d9ab5f
Your Name <you@example.com>
2023-08-29 11:06:46 +0800

    get data from server

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
 // const Note = (props) => {
@@ -7,11 +8,20 @@ import Note from "./components/Note";
 //   return <li>{note.content}</li>;
 // };
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
   const addNote = (event) => {
     event.preventDefault();
     console.log("button clicked", event.target); //即form
